4827c8ea7a6267c314a04602ee26016a86b253b6
minhgucodegym <roshannguyen125@gmail.com>
2023-12-08 19:24:59 +0700

    baitapgu

---
+++ w02/bt1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SystemTime
+{
+class bt1
+
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Nhap ten ban:");
+        string tenBan = Console.ReadLine();
+        Console.WriteLine("Xin chao: " + tenBan);
+        
+            
+        }
+        }
+}
---
+++ w02/bt2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace SystemTime;
+class Program
+    
+
+{
+   static void Main(string[] args)
+    {
+        float tienViet;
+        float tienDo = 23000;
+        Console.Write("Ban can doi bao nhieu $: ");
+    tienDo = float.Parse(Console.ReadLine());
+    float doiTien = tienDo*23000;
+    Console.WriteLine("So tien ban nhan duoc la:" + doiTien + "vnđ");
+
+
+    }
+}
