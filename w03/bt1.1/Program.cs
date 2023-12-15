5111afc117352d84b76ec50441600d1bb96028ed
minhgucodegym <roshannguyen125@gmail.com>
2023-12-15 19:16:02 +0700

    baitapgu

---
+++ w03/bt1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace SystemTime;
+
+    class Program;
+    {
+
+    static void Main (string []args)
+    {
+        Console.WriteLine("Vui long chon Menu:");
+        Console.WriteLine("1.Print the rectangle:");
+        Console.WriteLine("2.Print the rectangle:");
+        Console.WriteLine("3.Print isosceles triangle:");
+        Console.WriteLine("4.Exit:");
+
+        Console.WriteLine("Lua chon cua ban la:");
+        int choice;
+
+       choice = Int32.Parse(Console.ReadLine());
+
+
+    }
+    
+    }
+
+
---
+++ w03/thuchanh2.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+namespace SystemTime;
+class Program
+
+{
+    static void Main (string []args)
+    {
+        int number;
+        Console.WriteLine("Nhap so muon kiem tra:");
+
+        number = Int32.Parse(Console.ReadLine());
+
+        if(number < 2)
+    
+            Console.WriteLine( number + "Khong la SNT:");
+
+            else
+            {
+                int i =2;
+                bool check = true;
+
+                while(i <= Math.Sqrt(number))
+                {
+                    if(number%i ==0)
+                    {
+                        check = false;
+                        break;
+
+                    }
+                    i++;
+
+                }
+                if(check)
+                {
+                    Console.WriteLine(number +"La SNT!");
+
+                }
+                else
+                {
+                    Console.WriteLine(number +"Ko la SNT!");
+
+                }
+
+            }
+
+
+
+        
+
+
+    }
+}
